f9e54b5f8282dfa732c236d82e759962c72f100a
Lindao10 <zzdd2933@gmail.com>
2025-12-18 16:37:22 -0300

    Add character permissions and tiers to Lua file

--- RedzHubUI_COMPLETO_RedDark.lua
+++ RedzHubUI_COMPLETO_RedDark.lua
@@ -1,1 +1,67 @@
+local Tabela = {
 
+	PlayerPermission = {
+		["Gabrielqgjkdo"] = {Tier = 2000},
+	},
+
+	CharacterTier = {
+
+		["Enrico"] = {
+			Tier = 1,
+			Permission = {7809825005}
+		},
+
+		["DIO"] = {
+			Locked = {36651812,1569341667}
+		},
+
+		["Madara"] = {
+			Locked = {36651812,785068132,1569341667}
+		},
+
+		["Koku"] = {
+			Locked = {785068132,1569341667}
+		},
+
+		["Yori"] = {
+			Permission = {7809825005}
+		},
+
+		["Meliodas"] = {
+			Permission = {7809825005}
+		},
+
+		["Goku"] = {
+			Locked = {785068132,1569341667}
+		},
+
+		["TOP"] = {
+			Locked = {785068132,128677359,1569341667}
+		},
+
+		["Giorno"] = {
+			Tier = 1050,
+			Permission = {7809825005}
+		},
+
+		["Hakaishin"] = {
+			Locked = {36651812,128677359,785068132,629259254,1569341667}
+		},
+
+		["Boss"] = {
+			Locked = {785068132,36651812,7648333286,1569341667}
+		},
+
+		["JP6"] = {
+			Tier = 1,
+			Permission = {7809825005}
+		},
+
+		["Gojo"] = {
+			Tier = 1,
+			Permission = {7809825005}
+		},
+	}
+}
+
+return Tabela
